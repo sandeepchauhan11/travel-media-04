7e41b29550a8b964c0c565e85ccf8dac3ab50e7b
SANDEEPKUMAR CHAUHAN <sandeepchauhan71999@gmail.com>
2024-03-30 21:46:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RoutesManager from "./routes/Routes";
+import Loader from "./components/Loader";
+
+const App = () => {
+  return (
+    <>
+      <RoutesManager />
+    </>
+  );
+};
+
+export default App;
